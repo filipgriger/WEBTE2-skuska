42913237a3678457dcbbbdacf46ed9a5d0ac0232
s7rayn <adam.ruzicka.x@gmail.com>
2021-05-15 11:31:06 +0200

    translation

--- templates/testDoesntExist.php
+++ templates/testDoesntExist.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Test Not Found</title>
+    <title>Test nebol nájdený</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
 </head>
 <body class="py-5 text-center">
-    <h1 class="my-5">Test <span class="font-weight-bold px-2"><?=$_GET['testCode']?></span>doesn't exist or is inactive.</h1>
-    <a href="../index.php" class="btn btn-dark px-5">Return</a>
+    <h1 class="my-5">Test <span class="font-weight-bold px-2"><?=$_GET['testCode']?></span>neexistuje alebo nie je aktívny.</h1>
+    <a href="../index.php" class="btn btn-dark px-5">Návrat</a>
 </body>
 </html>
